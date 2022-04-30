7b6ac5cecb9a760b0020d791d660c503b62c6f5f
yewon.seo <dodnjs1241@naver.com>
2022-04-30 10:26:42 +0900

    Add files via upload

---
+++ day10/bitOper/bitOpper.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main() {
+	printf("%d\n", 10 & 11);
+	printf("%d\n", 10 | 11);
+	printf("%d\n", 10 ^ 11);
+	printf("%d\n", ~10);
+	printf("%d\n", ~9);
+	printf("%d\n", 10<<1);
+	printf("%d\n", 10 >>1);
+}
+
+
