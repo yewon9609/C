1d0bbbc67477cfbdc6ad5787d8e43660843806a9
yewon.seo <dodnjs1241@naver.com>
2022-04-30 10:32:22 +0900

    Add files via upload

---
+++ day11/oper/oper1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main() {
+
+	int result = 5 > 10;
+
+
+
+	printf("%d\n", 10 == 11);
+	printf("%d\n", !(10 == 11));
+	printf("%d\n", 10 == 10 && 10 < 3);
+	printf("%d\n", 10 == 10 || 10 < 3);
+	printf("%d\n", result);
+}
